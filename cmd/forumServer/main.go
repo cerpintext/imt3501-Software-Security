daaf879ffcee890a0407eb587c4a4cd5fa00d374
JonShard <jonems@stud.ntnu.no>
2018-09-20 12:44:26 +0200

    Now possible to request spesific webpages with GET request, any html file in the html folder in root, can be loaded by the server.

--- cmd/forumServer/main.go
+++ cmd/forumServer/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 const address = "127.0.0.1" // Localhost.
@@ -11,19 +12,26 @@ const port = "5000"         // Must be an open port. On linux open with $source 
 const htmlPath = "../html/"
 
 func defaultHandler(w http.ResponseWriter, r *http.Request) { // Default request handler handles domain/ requests.
-
+	w.Header().Set("Content-Type", "text/html")
 	fmt.Print("Received a request to /.")
 	switch r.Method {
+
 	case "GET":
-		data, err := ioutil.ReadFile(htmlPath + "index.html")
+
+		fileNames := strings.Split(r.URL.Path, "/")
+		fileName := fileNames[len(fileNames)-2] + ".html"
+		fmt.Printf("Http request for html file: %s", fileName)
+		data, err := ioutil.ReadFile(htmlPath + fileName)
 		if err != nil {
 			fmt.Print("Something went wrong: " + string(data))
 			panic(err)
 		}
 		fmt.Fprint(w, string(data))
 		break
+
 	case "POST":
-		fmt.Fprintf(w, "Hello world! You did a post request.") // Write hello world to respone writer.
+
+		fmt.Fprint(w, "Hello world! You did a post request.") // Write hello world to respone writer.
 		break
 	}
 }
