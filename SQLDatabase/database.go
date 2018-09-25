4036f5a8fc0d7b660906f71364ba9127588a5c1d
knaku <makval@live.no>
2018-09-26 00:50:23 +0200

    Copypasta working openDB funtion, with global *sql.DB #3

--- SQLDatabase/database.go
+++ SQLDatabase/database.go
@@ -1,8 +1,11 @@
 package database
 
 import (
+	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 )
+
+var db *sql.DB // global value for keeping database open
 
 type Category struct {
 }
@@ -17,7 +20,17 @@ type Message struct {
 }
 
 func openDB() {
+	var err error
+	db, err = sql.Open("mysql", "USERNAME:PASSWORD@tcp(IP-ADDRESS:PORT)/DATABASE")
+	if err != nil {
+		panic(err.Error()) // Implement proper handlig
+	}
 
+	// Open doesn't open a connection. Validate DSN data:
+	err = db.Ping()
+	if err != nil {
+		panic(err.Error()) // Implement proper handlig
+	}
 }
 
 func AddThread() {
