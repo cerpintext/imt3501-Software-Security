743d0eaf95214906d095f540d7626d014c20971f
knaku <makval@live.no>
2018-09-26 00:52:26 +0200

    AddThread() added, still needs proper error and DB handling #3

--- SQLDatabase/database.go
+++ SQLDatabase/database.go
@@ -8,9 +8,13 @@ import (
 var db *sql.DB // global value for keeping database open
 
 type Category struct {
+	name string
 }
 
 type Thread struct {
+	id       int
+	name     string
+	username string
 }
 
 type User struct {
@@ -33,8 +37,24 @@ func openDB() {
 	}
 }
 
+//	How to use
+//	AddMessage(Message{intId, "the message to be posten", \
+//		"timestamp on mysql accepted format e.g 1971-01-01 00:00:00 as a string", "parentMessage"})
 func AddThread() {
-	openDB()
+	openDB()                          // should find better way to handle db connection globally
+	if c, ok := class.(Message); ok { // type assert on it
+		stmtIns, err := db.Prepare("INSERT INTO Thread VALUES( ?, ?, ? )") // ? = placeholder
+		if err != nil {
+			panic(err.Error()) // Implement proper handlig
+		}
+		defer stmtIns.Close() // Close the statement when we leave function() / the program terminates
+
+		_, err = stmtIns.Exec(c.id, c.name, c.username) // Insert tuples (id, name, userName)
+		if err != nil {
+			panic(err.Error()) // Implement proper handlig
+		}
+	}
+	db.close() // should find better way to handle db connection globally
 }
 
 func AddUser(username string, passwordHash string) {
