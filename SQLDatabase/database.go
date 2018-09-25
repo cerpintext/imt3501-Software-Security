55a5429e0550b127b686da15d4ea47e16ab6a33b
knaku <makval@live.no>
2018-09-26 00:55:00 +0200

    AddUser() added, still needs proper error and DB handling #3

--- SQLDatabase/database.go
+++ SQLDatabase/database.go
@@ -18,6 +18,11 @@ type Thread struct {
 }
 
 type User struct {
+	username     string
+	email        string
+	passwordHash string
+	reputation   int
+	role         int
 }
 
 type Message struct {
@@ -57,8 +62,23 @@ func AddThread() {
 	db.close() // should find better way to handle db connection globally
 }
 
+//	How to use
+//	AddUser(Message{"userName", "email", "passwordHash" "reputation", "role"})
 func AddUser(username string, passwordHash string) {
-	openDB()
+	openDB()                          // should find better way to handle db connection globally
+	if c, ok := class.(Message); ok { // type assert on it
+		stmtIns, err := db.Prepare("INSERT INTO User VALUES( ?, ?, ?, ?, ? )") // ? = placeholder
+		if err != nil {
+			panic(err.Error()) // Implement proper handlig
+		}
+		defer stmtIns.Close() // Close the statement when we leave function() / the program terminates
+		// Insert tuples (username, email, passwordHash, reputation, role)
+		_, err = stmtIns.Exec(c.username, c.email, c.passwordHash, c.reputation, c.role)
+		if err != nil {
+			panic(err.Error()) // Implement proper handlig
+		}
+	}
+	db.close() // should find better way to handle db connection globally
 }
 
 func AddMessage(message string, username string) {
