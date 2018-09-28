f243d5ee3a93431ed6fece9adc885469d9083dd8
Martin <martiakv@stud.ntnu.no>
2018-09-28 12:18:45 +0200

    Uses complete struct again, added thread error handling

--- SQLDatabase/database.go
+++ SQLDatabase/database.go
@@ -1,4 +1,4 @@
-package database
+package main
 
 import (
 	"database/sql"
@@ -9,29 +9,29 @@ import (
 var db *sql.DB // global value for keeping database open
 
 type Category struct {
-	name string
+	Name string
 }
 
 type Thread struct {
-	//	id       int
-	name     string
-	username string
+	Id       int
+	Name     string
+	Username string
 }
 
 type User struct {
-	username     string
-	email        string
-	passwordHash string
-	//	reputation   int
-	//	role         int
+	Username     string
+	Email        string
+	PasswordHash string
+	Reputation   int
+	Role         int
 }
 
 type Message struct {
-	//	id            int
-	message string
-	//	timestamp     string
-	username      string
-	parentMessage int
+	Id            int
+	Message       string
+	Timestamp     string
+	Username      string
+	ParentMessage int
 }
 
 func openDB() {
@@ -44,61 +44,57 @@ func openDB() {
 	// Open doesn't open a connection. Validate DSN data:
 	err = db.Ping()
 	if err != nil {
-		log.Fatalln("Could not connect to the database")
+		//log.Fatalln("Could not connect to the database")
+		errorHandling(err, "ping")
 	}
 }
 
+//	Only uses fields Name and Username
 //	How to use
 //	AddThread(Thread{intId, "name", "existingUsername"})
-func AddThread(class interface{}) {
-	if c, ok := class.(Thread); ok { // type assert on it
-		stmtIns, err := db.Prepare("INSERT INTO Thread (`name`, `username`) VALUES( ?, ? )") // ? = placeholder
-		if err != nil {
-			panic(err.Error()) // Implement proper handlig
-		}
-		defer stmtIns.Close() // Close the statement when we leave function() / the program terminates
-
-		_, err = stmtIns.Exec(c.name, c.username) // Insert tuples (id, name, userName)
-		if err != nil {
-			errorHandling(err, "thread")
-		}
+func AddThread(c Thread) {
+	stmtIns, err := db.Prepare("INSERT INTO Thread (`name`, `username`) VALUES( ?, ? )") // ? = placeholder
+	if err != nil {
+		log.Fatalln("wut?")
+		panic(err.Error()) // Implement proper handlig
+	}
+	defer stmtIns.Close()                     // Close the statement when we leave function() / the program terminates
+	_, err = stmtIns.Exec(c.Name, c.Username) // Insert tuples (name, userName)
+	if err != nil {
+		errorHandling(err, "thread")
 	}
 }
 
+//	Only uses fields Username, Email, and passwordHash
 //	How to use
 //	AddUser(User{"userName", "email", "passwordHash" reputation, role})
-func AddUser(class interface{}) {
-	if c, ok := class.(User); ok { // type assert on it
-		//	if checkUserExists(c.username) == true {
-		stmtIns, err := db.Prepare("INSERT INTO User (`username`, `email`, `passwordHash`) VALUES( ?, ?, ? )") // ? = placeholder
-		if err != nil {
-			panic(err.Error()) // Implement proper handlig
-		}
-		defer stmtIns.Close() // Close the statement when we leave function() / the program terminates
-		// Insert tuples (username, email, passwordHash, reputation, role)
-		_, err = stmtIns.Exec(c.username, c.email, c.passwordHash)
-		if err != nil {
-			errorHandling(err, "user")
-		}
-		//	}
+func AddUser(c User) {
+	stmtIns, err := db.Prepare("INSERT INTO User (`username`, `email`, `passwordHash`) VALUES( ?, ?, ? )") // ? = placeholder
+	if err != nil {
+		log.Fatalln("Could not add new user, we're having issues the database")
+	}
+	defer stmtIns.Close() // Close the statement when we leave function() / the program terminates
+	// Insert tuples (username, email, passwordHash, reputation, role)
+	_, err = stmtIns.Exec(c.Username, c.Email, c.PasswordHash)
+	if err != nil {
+		errorHandling(err, "user")
 	}
 }
 
+//	Only uses fields Message, Username and ParentMessage
 //	How to use
 //	AddMessage(Message{intId, "the message to be posted", \
 //		"timestamp on mysql accepted format e.g 1971-01-01 00:00:00 as a string", "username", parentMessage})
-func AddMessage(class interface{}) {
-	if c, ok := class.(Message); ok { // type assert on it
-		stmtIns, err := db.Prepare("INSERT INTO Message (`message`, `username`, `parentmessage`) VALUES( ?, ?, ? )") // ? = placeholder
-		if err != nil {
-			panic(err.Error()) // Implement proper handlig
-		}
-		defer stmtIns.Close() // Close the statement when we leave function() / the program terminates
-		// Insert tuples (message, username, parentMessage)
-		_, err = stmtIns.Exec(c.message, c.username, c.parentMessage)
-		if err != nil {
-			errorHandling(err, "message")
-		}
+func AddMessage(c Message) {
+	stmtIns, err := db.Prepare("INSERT INTO Message (`message`, `username`, `parentmessage`) VALUES( ?, ?, ? )") // ? = placeholder
+	if err != nil {
+		log.Fatalln("Could not add new message, we're having issues the database")
+	}
+	defer stmtIns.Close() // Close the statement when we leave function() / the program terminates
+	// Insert tuples (message, username, parentMessage)
+	_, err = stmtIns.Exec(c.Message, c.Username, c.ParentMessage)
+	if err != nil {
+		errorHandling(err, "message")
 	}
 }
 
@@ -113,10 +109,14 @@ func errorHandling(err error, function string) {
 		log.Println("Something strange happend a thread with this ID already exists")
 	} else if mysqlErr.Number == 1062 && function == "message" { // Duplicate message
 		log.Println("Something strange happend a message with this ID already exists")
-	} else if mysqlErr.Number == 1452 && function == "message" && string(runes[135:143]) == "username" { // Non existent user
+	} else if mysqlErr.Number == 1452 && function == "message" && string(runes[134:143]) == "username" { // Non existent user
 		log.Println("Hmm, that's not supposed to happen. User not found when posting message")
-	} else if mysqlErr.Number == 1452 && function == "message" && string(runes[135:148]) == "parentmessage" { // Non existent parent message
+	} else if mysqlErr.Number == 1452 && function == "message" && string(runes[134:148]) == "parentmessage" { // Non existent parent message
 		log.Println("Hmm, that's not supposed to happen. Parent message message not found") // output might need to be changed
+	} else if mysqlErr.Number == 1452 && function == "thread" && string(runes[133:141]) == "username" { // Non existent parent message
+		log.Println("Hmm, that's not supposed to happen. Username does not exist when posting a new thread") // output might need to be changed
+	} else if function == "ping" { // Non existent parent message
+		log.Println("Could not connect to the database") // output might need to be changed
 	} else { // Unkown error
 		//log.Println(string(runes[135:148]))
 		panic(err.Error())
